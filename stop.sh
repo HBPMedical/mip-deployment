6ddcc3d02004fbb017e16707761b49ef6f1e9ab2
ThanKarab <tkarabatsis@hotmail.com>
2020-02-20 23:06:28 +0200

    Stop script added and fixes.

--- stop.sh
+++ stop.sh
@@ -4,14 +4,5 @@ set -o pipefail # trace ERR through pipes
 set -o errtrace # trace ERR through 'time command' and other functions
 set -o errexit  ## set -e : exit the script if any statement returns a non-true return value
 
-if [ ! -d "logs" ]; then
-        sudo mkdir logs
-fi
-
-sudo chmod a+rwx logs
-
 echo -e "\nRemoving previous services..."
 docker-compose --project-name mip down
-
-echo -e "\nDeploy Services..."
-docker-compose --project-name mip up -d
