faea4b1df9689ac15990c5f3da2fd2628ead41aa
Dimitri Papadopoulos <3234522+DimitriPapadopoulos@users.noreply.github.com>
2020-02-26 11:01:38 +0100

    run.sh must be run with sudo

--- run.sh
+++ run.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
 
+if [ `id -u` -ne 0 ]; then
+    echo "Please run this script with sudo"
+    exit 1
+fi
+
 set -o pipefail # trace ERR through pipes
 set -o errtrace # trace ERR through 'time command' and other functions
 set -o errexit  ## set -e : exit the script if any statement returns a non-true return value
 
 if [ ! -d "logs" ]; then
-        sudo mkdir logs
+        mkdir logs
 fi
 
-sudo chmod a+rwx logs
+chmod a+rwx logs
 
 echo -e "\nRemoving previous services..."
 docker-compose --project-name mip down
