6d91d9f10d63a3267d15e7775e9cfe3a0bd6f0a8
sofiakarb <karvoun@di.uoa.gr>
2020-03-25 13:52:47 +0200

    source .env

--- Federation/run.sh
+++ Federation/run.sh
@@ -8,7 +8,7 @@ echo -e "\nYou need to configure the 'EXAREME_IP' variable. It is the IP where E
 echo -e "\nYou need to configure the 'PUBLIC_MIP_IP' variable. It is the IP where MIP will be visible from."
 ../config/check_env_variabe_IP.sh .env PUBLIC_MIP_IP
 
-. ./.env
+. .env
 
 # Creating logs folder
 if [ ! -d "logs" ]; then
