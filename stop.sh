cde69b092638417b591fd5e761be8a727a8f15fe
sofiakarb <karvoun@di.uoa.gr>
2020-03-27 16:18:09 +0200

    update stop.sh for Local

--- stop.sh
+++ stop.sh
@@ -4,5 +4,14 @@ set -o pipefail # trace ERR through pipes
 set -o errtrace # trace ERR through 'time command' and other functions
 set -o errexit  ## set -e : exit the script if any statement returns a non-true return value
 
+cat ./.env >> ./.env_file ; echo "" >> ./.env_file ; cat ./.versions_env >> ./.env_file
+return=$?
+if [[ ${return} -ne 0 ]]; then
+    echo -e "\nMake sure .env file exists.Exiting.." >&2
+    exit 1
+else
+    :
+fi
+
 echo -e "\nRemoving previous services..."
-docker-compose --project-name mip down
+docker-compose --project-name mip --env-file ./.env_file down
