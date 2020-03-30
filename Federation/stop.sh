112b552f765e7f2b51ba166b097f5e4aeaed9478
sofiakarb <karvoun@di.uoa.gr>
2020-03-30 13:08:06 +0300

    --log-level ERRO for docker-compose down

--- Federation/stop.sh
+++ Federation/stop.sh
@@ -9,21 +9,11 @@ if [ `id -u` -ne 0 ]; then
     exit 1
 fi
 
-if [[ -e ../.IPs_env ]]; then
-    :
-else
-    echo -e "\nYou need to configure the 'EXAREME_IP' variable. It is the IP where Exareme master is currently running."
-    ../config/check_env_variabe_IP.sh ../.IPs_env EXAREME_IP
-
-    echo -e "\nYou need to configure the 'PUBLIC_MIP_IP' variable. It is the IP where MIP will be visible from."
-    ../config/check_env_variabe_IP.sh ../.IPs_env PUBLIC_MIP_IP
-
-fi
-cat ../.IPs_env >> .env ; echo "" >> .env ; cat ../.versions_env >> .env
+cat ../.versions_env >> .env
 
 echo -e "\nRemoving previous services..."
 
-docker-compose --project-name mip_federation down
+docker-compose --project-name mip_federation --log-level ERROR down
 docker_compose_down=$?
 if [[ ${docker_compose_down} -ne 0 ]]; then
     echo -e "\nAn error has occurred while removing services and networks.Exiting.." >&2
--- stop.sh
+++ stop.sh
@@ -12,17 +12,10 @@ fi
 set -o pipefail # trace ERR through pipes
 set -o errtrace # trace ERR through 'time command' and other functions
 
-if [[ -e ./.IPs_env ]]; then
-    :
-else
-    echo "You need to configure the 'PUBLIC_MIP_IP' variable. It is the IP where MIP will be visible from. If you only want to install it on your local machine, you can initialize it with 127.0.0.1"
-    ./config/check_env_variabe_IP.sh .IPs_env PUBLIC_MIP_IP
-fi
-
-cat ./.IPs_env >> .env ; echo "" >> .env ; cat ./.versions_env >> .env
+cat ./.versions_env >> .env
 
 echo -e "\nRemoving previous services..."
-docker-compose --project-name mip down
+docker-compose --project-name mip --log-level ERROR down
 docker_compose_down=$?
 if [[ ${docker_compose_down} -ne 0 ]]; then
     echo -e "\nAn error has occurred while removing services and networks.Exiting.." >&2
