705e096c3df349accd65c1dc7160c58211691e21
sofiakarb <karvoun@di.uoa.gr>
2020-03-30 12:28:52 +0300

    fix

--- stop.sh
+++ stop.sh
@@ -16,7 +16,7 @@ if [[ -e ./.IPs_env ]]; then
     :
 else
     echo "You need to configure the 'PUBLIC_MIP_IP' variable. It is the IP where MIP will be visible from. If you only want to install it on your local machine, you can initialize it with 127.0.0.1"
-    ./config/check_env_variabe_IP.sh ./.IPs_env PUBLIC_MIP_IP
+    ./config/check_env_variabe_IP.sh .IPs_env PUBLIC_MIP_IP
 fi
 
 cat ./.IPs_env >> .env ; echo "" >> .env ; cat ./.versions_env >> .env
