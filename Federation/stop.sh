f7b12757e1e12265bbe40cd820c8f3a4c4077271
sofiakarb <karvoun@di.uoa.gr>
2020-03-30 12:17:46 +0300

    update stop.sh

--- Federation/stop.sh
+++ Federation/stop.sh
@@ -9,21 +9,21 @@ if [ `id -u` -ne 0 ]; then
     exit 1
 fi
 
-if [[ -e ../.env ]]; then
+if [[ -e ../.IPs_env ]]; then
     :
 else
     echo "You need to configure the 'PUBLIC_MIP_IP' variable. It is the IP where MIP will be visible from. If you only want to install it on your local machine, you can initialize it with 127.0.0.1"
-    ../config/check_env_variabe_IP.sh ../.env PUBLIC_MIP_IP
+    ../config/check_env_variabe_IP.sh ../.IPs_env PUBLIC_MIP_IP
 
     echo -e "\nYou need to configure the 'PUBLIC_MIP_IP' variable. It is the IP where MIP will be visible from."
-    ../config/check_env_variabe_IP.sh ../.env PUBLIC_MIP_IP
+    ../config/check_env_variabe_IP.sh ../.IPs_env PUBLIC_MIP_IP
 
 fi
-cat ../.env >> ./.env_file ; echo "" >> ./.env_file ; cat ../.versions_env >> ./.env_file
+cat ../.IPs_env >> .env ; echo "" >> .env ; cat ../.versions_env >> .env
 
 echo -e "\nRemoving previous services..."
 
-docker-compose --project-name mip_federation --env-file ./.env_file down
+docker-compose --project-name mip_federation down
 docker_compose_down=$?
 if [[ ${docker_compose_down} -ne 0 ]]; then
     echo -e "\nAn error has occurred while removing services and networks.Exiting.." >&2
@@ -32,4 +32,4 @@ else
     :
 fi
 
-rm ./.env_file
+rm .env
--- stop.sh
+++ stop.sh
@@ -12,14 +12,16 @@ fi
 set -o pipefail # trace ERR through pipes
 set -o errtrace # trace ERR through 'time command' and other functions
 
-if [[ -e ./.env ]]; then
+if [[ -e ./.IPs_env ]]; then
     :
 else
     echo "You need to configure the 'PUBLIC_MIP_IP' variable. It is the IP where MIP will be visible from. If you only want to install it on your local machine, you can initialize it with 127.0.0.1"
     ./config/check_env_variabe_IP.sh ./.env PUBLIC_MIP_IP
 fi
 
-cat ./.env >> ./.env_file ; echo "" >> ./.env_file ; cat ./.versions_env >> ./.env_file
+cat ./.IPs_env >> .env ; echo "" >> .env ; cat ./.versions_env >> .env
 
 echo -e "\nRemoving previous services..."
-docker-compose --project-name mip --env-file ./.env_file down
+docker-compose --project-name mip down
+
+rm .env
