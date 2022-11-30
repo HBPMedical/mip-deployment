5f373ca67f683b3a89f6559053509bf7afbf5483
kfilippopolitis <kostasfilippop@gmail.com>
2022-11-30 14:46:31 +0200

    Problematic data model

--- tests/setup/run.sh
+++ tests/setup/run.sh
@@ -6,10 +6,12 @@ cat ../../.versions_env >> .env_with_versions
 cd backend_components/
 docker-compose down
 docker-compose --env-file ../.env_with_versions up -d
-poetry run inv setup-dbs
+#poetry run inv setup-dbs
+poetry run mipdb init --port 50001
+poetry run mipdb load-folder tbi --port 50001
 
 cd ../
-cd frontend_components
+cd frontend_components/
 docker-compose down
 docker-compose --env-file ../.env_with_versions up -d
 
