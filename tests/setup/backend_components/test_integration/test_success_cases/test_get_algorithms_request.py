1918e09798b682e306e081b97361395a0b0d4b78
K.Filippopolitis <56073635+KFilippopolitis@users.noreply.github.com>
2023-01-12 17:39:03 +0200

    New mipdb version (#131)
    New mipdb and MIP-Engine version.

--- tests/setup/backend_components/test_integration/test_success_cases/test_get_algorithms_request.py
+++ tests/setup/backend_components/test_integration/test_success_cases/test_get_algorithms_request.py
@@ -10,21 +10,18 @@ def test_get_algorithms_request():
     assert response.status_code == 200
     print(f"Algorithms result-> {response.text}")
     algorithms = json.loads(response.text)
-    assert len(algorithms) == 31
+    assert len(algorithms) == 32
 
     mip_engine_algorithms = [
         algorithm for algorithm in algorithms if algorithm["type"] == "mipengine"
     ]
 
-    assert len(mip_engine_algorithms) == 11
+    assert len(mip_engine_algorithms) == 12
 
     exareme_engine_algorithms = [
         algorithm
         for algorithm in algorithms
         if algorithm["type"] not in ["mipengine", "workflow"]
     ]
-    for algorithm in exareme_engine_algorithms:
-        print(algorithm["name"])
-        print("\n")
     print(f"exareme_engine_algorithms-> {exareme_engine_algorithms}")
     assert len(exareme_engine_algorithms) == 20
