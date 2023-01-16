9e95c54b4ea518966f6f11cd1de07c6e75ed6e2a
ThanKarab <tkarabatsis@hotmail.com>
2023-01-16 19:22:24 +0200

    Minor fixes for local deployment.
    Poetry update on the test poetry.lock
    Downgraded mip version since there is still an incompatibility on histograms.
    Minor improvements on start/stop setup scripts.

--- tests/stop.sh
+++ tests/stop.sh
@@ -1,4 +1,4 @@
 #!/bin/env bash
 
-docker-compose -f backend_components/docker-compose.yml down
-docker-compose -f frontend_components/docker-compose.yml down
+docker-compose --log-level ERROR -f backend_components/docker-compose.yml down
+docker-compose --log-level ERROR -f frontend_components/docker-compose.yml down
