381fc60be8c54f1b7839c117570d5cb63c138aeb
K.Filippopolitis <56073635+KFilippopolitis@users.noreply.github.com>
2023-01-17 12:40:10 +0200

    Fix for stop.sh. (#135)

--- tests/stop.sh
+++ tests/stop.sh
@@ -1,4 +1,4 @@
 #!/bin/env bash
 
-docker-compose --env-file ../.env_with_versions -f backend_components/docker-compose.yml down
-docker-compose --env-file ../.env_with_versions -f frontend_components/docker-compose.yml down
+docker-compose --log-level ERROR  -f backend_components/docker-compose.yml down
+docker-compose --log-level ERROR  -f frontend_components/docker-compose.yml down
