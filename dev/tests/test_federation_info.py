79ebec888610a16c2fdffeb79f1f2232719679c6
K.Filippopolitis <56073635+KFilippopolitis@users.noreply.github.com>
2024-08-23 17:18:08 +0300

    Added a volume for Portalbackend logs. (#168)
    Co-authored-by: Kostas Filippopolitis <kfilippopolitis@athenarc.gr>
    Co-authored-by: Julien D <un.common366@passinbox.com>

--- dev/tests/test_federation_info.py
+++ dev/tests/test_federation_info.py
@@ -6,7 +6,7 @@ EXPERIMENT_AUDIT_ENTRY_IDENTIFIER = " - EXPERIMENT_FINISHED - "
 
 
 def test_federation_info():
-    cmd = f"docker logs dev_portalbackend_1 | python3 ../federation_info.py show-portal-backend-audit-entries"
+    cmd = f"docker logs dev-portalbackend-1 | python3 ../federation_info.py show-portal-backend-audit-entries"
     res = subprocess.run(
         cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
     )
